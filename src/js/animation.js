ce19ca69b58b51188a61f98045c4a2a130c8068c
NbYb <NbYb@users.noreply.github.com>
2020-09-12 18:05:39 +1000

    build/css2scss

--- src/js/animation.js
+++ src/js/animation.js
@@ -20,6 +20,7 @@ const add_animation = (ith_title) => {
 
     let current_box = document.getElementsByClassName('page_active')[0];
     let target_box = document.getElementsByClassName(pages_reverse[ith_title])[0];
+    let sliding_window = document.getElementsByClassName('sliding_window')[0];
     console.log(target_box);
 
     Object.keys(pages).forEach(function (key) {
@@ -27,35 +28,72 @@ const add_animation = (ith_title) => {
             current_box_index = pages[key];
         }
     });
-    if (target_box !== current_box) {
-        target_box.classList.remove("page_inactive");
-        target_box.classList.add("page_active");
-        current_box.classList.remove("page_active");
-        current_box.classList.add("page_inactive");
+    console.log(current_box_index);
+    // if (target_box !== current_box) {
+    //     target_box.classList.remove("page_inactive");
+    //     target_box.classList.add("page_active");
+    //     current_box.classList.remove("page_active");
+    //     current_box.classList.add("page_inactive");
+    // }
+    if (current_box_index < ith_title) {
+        console.log("left");
+        sliding_window.classList.add("move_to_left");
+
+
+        let timer_1 = setTimeout(() => {
+            current_box.classList.remove("page_active");
+            target_box.classList.add("page_active");
+            sliding_window.setAttribute("style", "left:-100%")
+            sliding_window.classList.remove("move_to_left");
+            clearTimeout(timer_1);
+        }, 1500);
     }
+
+    else if(current_box_index > ith_title){
+        console.log(current_box_index);
+        console.log(ith_title);
+        console.log("right");
+        sliding_window.classList.add("move_to_right");
+
+
+        let timer_2 = setTimeout(() => {
+            current_box.classList.remove("page_active");
+            target_box.classList.add("page_active");
+            sliding_window.removeAttribute("style", "left:-100%");
+            sliding_window.classList.remove("move_to_right");
+            clearTimeout(timer_2);
+        }, 1500);
+    }
+
     // switch (true) {
     //     case (current_box_index < ith_title):
-    //         current_box.classList.add("move_to_left");
+    //         console.log("left");
+    //         sliding_window.classList.add("move_to_left");
 
 
     //         let timer_1 = setTimeout(() => {
     //             current_box.classList.remove("page_active");
-    //             current_box.classList.add("page_inactive");
-    //             current_box.classList.remove("move_to_left");
-    //             clearTimeout(timer_1);
-    //         }, 500);
-    //         let timer_2 = setTimeout(() => {
-    //             target_box.classList.remove("page_inactive");
     //             target_box.classList.add("page_active");
-    //             target_box.setAttribute("style", "left:100%")
-    //             target_box.classList.add("move_to_left");
-    //             let timer_3 = setTimeout(() => {
-    //                 target_box.classList.remove("move_to_left");
-    //                 target_box.removeAttribute("style", "left:100%");
-    //                 clearTimeout(timer_3);
-    //             }, 500);
+    //             sliding_window.setAttribute("style", "left:-100%")
+    //             sliding_window.classList.remove("move_to_left");
+    //             clearTimeout(timer_1);
+    //         }, 1500);
+
+    //     case (current_box_index > ith_title):
+    //         console.log(current_box_index);
+    //         console.log(ith_title);
+    //         console.log("right");
+    //         sliding_window.classList.add("move_to_right");
+
+
+    //         let timer_2 = setTimeout(() => {
+    //             current_box.classList.remove("page_active");
+    //             target_box.classList.add("page_active");
+    //             sliding_window.setAttribute("style", "left:100%")
+    //             sliding_window.classList.remove("move_to_right");
     //             clearTimeout(timer_2);
-    //         }, 600);
+    //         }, 1500);
+
     // };
 
 
